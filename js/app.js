422a0be2f7ef4f207a014eb144aa7e13191db720
valeriavalles <valeria.sv@outlook.es>
2017-12-25 15:28:43 -0500

    filtro/maquetado

--- js/app.js
+++ js/app.js
@@ -1,6 +1,19 @@
-$(function(){
+$(document).ready(function() {
+	setTimeout(function() {
+		window.location.href = 'views/contenido.html';
+    }, 4000);
 
     setTimeout(function() {
-        $('.fly-in-text').removeClass('hidden');
+        $('.fly-in-text').removeClass('oculto')
+       /* $('.welcome').fadeOut(10000);*/
+    },700 );
+    
+});
+/*$(function(){
+
+    setTimeout(function() {
+        $('.fly-in-text').removeClass('oculto')
+        $('.welcome').fadeOut(10000);
     },500 );
-})
+        
+})*/
