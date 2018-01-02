93243c20061fec8c879bda46ed282c7b82b0c275
valeriavalles <valeria.sv@outlook.es>
2018-01-02 11:08:56 -0500

    actualizando ultimos cambios/agrgando readme

--- js/conten.js
+++ js/conten.js
@@ -1,40 +1,45 @@
 $(document).ready(function() {
-
-  $('.men').on('mouseover',function() {
-    var valorAtrr= $(this).attr('data-imag');
-    var texto= $('#'+ valorAtrr).addClass('hola');
-  //  $('.men').append(texto);
+/* ------------------EVENTO MOUSEOVER----------------*/
+  $('.men').on('mouseover', function() {
+    var valorAtrr = $(this).attr('data-imag');
+    var texto = $('#' + valorAtrr).addClass('hola');
+    //  $('.men').append(texto);
     console.log(valorAtrr);
-    });
+  });
+  $('.men').on('click', function() {
+    
+  });
 
+  /* -----------FUNCION PARA BUSCAR TIPO DE COMIDA--------*/
   $('#search').on('click', function() {
     var area = $('.buscar');
     event.preventDefault();
    
     switch (true) {
     case area.val() === 'comida peruana':
-     // $('.menus').text('gracias por nada');
-     $('.men')
-    .attr('src','../assets/images/uno.jpg')
-    .addClass('img-thumbnail'); 
+      mostrarImag();
       break;
-    case area.val() === 'italiana':
-    $('.men')
-    .attr('src','../assets/images/dos.jpg'); 
+    case area.val() === 'comida italiana':
+      $('.men')
+        .attr('src', '../assets/images/dos.jpg');
+      // mostrarImag(); 
       break;
-    case area.val() === 'mexicana':
-    $('.men')
-    .attr('src','../assets/images/tres.jpg');
+    case area.val() === 'comida mexicana':
+      $('.men')
+        .attr('src', '../assets/images/tres.jpg');
+      // mostrarImag();
       break;
     default:
       alert('Disculpe aun estoy aprendiendo!');
     }
-    // event.preventDefault();
-
   });
- 
-  
 
   
+  // probando con funciones //
+  function mostrarImag() {
+    $('.men')
+      .attr('src', '../assets/images/uno.jpg')
+      .addClass('img-thumbnail');
+  }
 });
 
---
+++ readme.md
@@ -0,0 +1,28 @@
+## Foodmap webApp
+
+
+Foodmap es una webApp, Brindado a usuarios que solicitan servicio de todo tipo de comidas a restaurantes que esten aledaños, por medio del map que les permite ver la ubicacion exacta y precios e tipos de comidas que ofrecen.
+
+Por la cual se nos pidio desarrollar como parte de nuestro retos de codigo en Laboratoria y parte del reto consiste en  dar  la interaccion entre web e usuario. 
+
+ #### HERRAMIENTAS UTILIZADAS 
+
+* HTML :
+  >Parte del maquetado  
+
+* CSS :
+  >Estilos: colores/tamaños 
+
+* JQUERY/JAVASCRIPT VANILLA : 
+  > Parte de la funcionalidad 
+
+  
+#### Vistas de la aplicacion
+
+> vista splash
+
+![ Texto alter](assets/images/splash.PNG)
+
+> Vista principal
+
+![ Texto alter](assets/images/foodmap.PNG)
